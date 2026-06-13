3b3860f678690c42b861ca4e8c40e5339bedb833
Sreya A A <sreyaaa0213@gmail.com>
2026-06-13 23:48:03 +0530

    Update bot.py
    changed back to Kalpatta, line 39

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def get_weather(city="Cheeral"):
     except Exception as e:
         return f"Weather unavailable ({e})"
 
-def get_weather2(city="Cherrapunji"):
+def get_weather2(city="Kalpatta"):
     api_key=os.environ.get("OPENWEATHER_API_KEY")
     url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     data=requests.get(url).json()
